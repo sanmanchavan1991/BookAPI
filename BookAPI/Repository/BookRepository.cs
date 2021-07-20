0d4967549319935b9d3fef2ada80845ca588ee16
sanman chavan <sanman chavan@LAPTOP-D54PQQ4H>
2021-07-20 16:37:50 +0530

    Add project files.

---
+++ BookAPI/Repository/BookRepository.cs
@@ -0,0 +1,57 @@
+﻿using BookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext _context;
+
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Book> Create(Book book)
+        {
+            var newbook = new Book
+            {
+                //   Id = 1,  //Don't need to write this
+                Author = book.Author,
+                Title = book.Title,
+                Description = book.Description
+            };
+            _context.Books.Add(newbook);
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        public async Task Delete(int id)
+        {
+            var bookToDelete = await _context.Books.FindAsync(id);
+            _context.Books.Remove(bookToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task Update(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
